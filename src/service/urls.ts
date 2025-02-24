4444bbee45b06bc1f70b94f0aa8b56010d4c895e
Mert Özbolat <mertozbolat008@gmail.com>
2025-02-24 12:28:34 +0300

    update

--- src/service/urls.ts
+++ src/service/urls.ts
@@ -1,4 +1,22 @@
 export const BASE_URL = 'https://fakestoreapi.com/';
+
 export const PRODUCTS_URL = {
     ALL_PRODUCTS: 'products'
+}
+
+export const CART_URLS = {
+    CARTS: 'carts',
+    USER_CART: 'carts/user'
+}
+
+export const USER_URLS = {
+    USERS: 'users'
+}
+
+export const AUTH_URLS = {
+    LOGIN: 'auth/login'
+}
+
+export const CATEGORIES_URLS = {
+    CATEGORIES: 'products/categories'
 }
