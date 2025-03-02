8ad738b299a571535bf90e77665ec0449b4041e0
Mert Özbolat <mertozbolat008@gmail.com>
2025-03-02 15:43:39 +0300

    update

--- src/screens/products/productDetail.tsx
+++ src/screens/products/productDetail.tsx
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: Colors.PRIMARY,
         marginVertical: 10,
-        fontWeight: '600'
+        fontWeight: '600',
+        textDecorationLine: 'underline'
     }
 })
