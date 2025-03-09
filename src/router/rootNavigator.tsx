5bc569de9510e20c707ac720ef8ec0e9e5a61ce6
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-03-09 21:01:33 +0300

    update

--- src/router/rootNavigator.tsx
+++ src/router/rootNavigator.tsx
@@ -5,19 +5,23 @@ import ProductList from '../screens/products';
 import ProductDetail from '../screens/products/productDetail';
 import { Colors } from '../theme/colors';
 import Login from '../screens/auth/login';
-import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator: React.FC = () => {
+
+    const getState = async () => {
+        await AsyncStorage.getItem('token')
+    }
+
     return (
         <Stack.Navigator
             screenOptions={{
                 headerBackTitle: 'Geri',
                 headerTintColor: Colors.BLACK
-            }}
-        >
+            }}>
             <Stack.Screen
                 options={{ headerShown: false }}
                 name={TABNAVIGATOR.TABNAVIGATOR} component={TabNavigator} />
