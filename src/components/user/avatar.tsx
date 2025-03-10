297939413930e58ea193634fa84a972f5bfef785
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-03-10 14:29:44 +0300

    update

--- src/components/user/avatar.tsx
+++ src/components/user/avatar.tsx
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AvatarProps } from '../../models/ui/avatarProps'
 
-const Avatar: React.FC = () => {
+const Avatar: React.FC<AvatarProps> = ({ name, surname, image, size }) => {
     return (
-        <View>
-            <Text>Avatar</Text>
+        <View style={styles.container}>
+            <Text style={{ fontSize: 30 }}>{ }</Text>
         </View>
     )
 }
 
 export default Avatar
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 50
+    }
+})
